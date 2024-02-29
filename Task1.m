1eff916676f9c00af29aea7c8a2cadb9e07ccb13
james taylor <jameset701@gmail.com>
2024-02-29 16:24:03 +0000

    fixed it works decently one of you make a trajectory function please

--- Task1.m
+++ Task1.m
@@ -126,20 +126,19 @@ function IK_final = choose_kinematic(joint_angles)
     j2=rad_to_j2(joint_angles(2,2)); 
     j3=rad_to_j3(joint_angles(2,3));
     j4=rad_to_j4(joint_angles(2,4));
-    
-    if (j2 < 760) || (j2 > 3290) || (j3 < 695) || (j3 > 3060) || (j4 < 820) || (j4 > 3450) || any(isnan(joint_angles(1,:)))
-        %warning('IK using positive cosine component out of range')
+    if (j2 < 760) || (j2 > 3290) || (j3 < 695) || (j3 > 3060) || (j4 < 820) || (j4 > 3450)
+        warning('IK using elbow up out of range')
         j1=rad_to_j1(joint_angles(1,1));
         j2=rad_to_j2(joint_angles(1,2)); 
         j3=rad_to_j3(joint_angles(1,3));
         j4=rad_to_j4(joint_angles(1,4));
-        if (j2 < 760) || (j2 > 3290) || (j3 < 695) || (j3 > 3060) || (j4 < 820) || (j4 > 3450) || any(isnan(joint_angles(2,:)))
+        if (j2 < 760) || (j2 > 3290) || (j3 < 695) || (j3 > 3060) || (j4 < 820) || (j4 > 3450)
             error('Joint angles are out of range for both IK solvers');
         else
-            IK_final = joint_angles(2,:);
+            IK_final = joint_angles(1,:);
         end
     else
-        IK_final = joint_angles(1,:);
+        IK_final = joint_angles(2,:);
     end
 end
 
@@ -150,16 +149,16 @@ function IK = inverse_kinematic(x_ef,y_ef,z_ef,phi)
     r_ef = sqrt(x_ef^2+y_ef^2);
     r_2 = r_ef-0.126*cos(phi);
     z_2 = z_ef-0.126*sin(phi);
-    theta3_plus = acos((r_2^2+z_2^2-0.13^2-0.124^2)/(2*0.13*0.124))
-    theta3_minus = -acos((r_2^2+z_2^2-0.13^2-0.124^2)/(2*0.13*0.124))
+    theta3_plus = acos((r_2^2+z_2^2-0.13^2-0.124^2)/(2*0.13*0.124));
+    theta3_minus = -acos((r_2^2+z_2^2-0.13^2-0.124^2)/(2*0.13*0.124));
     costheta2_plus = (r_2*(0.13+0.124*cos(theta3_plus))+z_2*0.124*sin(theta3_plus))/(r_2^2+z_2^2);
     costheta2_minus = (r_2*(0.13+0.124*cos(theta3_minus))+z_2*0.124*sin(theta3_minus))/(r_2^2+z_2^2);
-    sintheta2_plus = -sqrt(1-costheta2_plus^2)
-    sintheta2_minus = sqrt(1-costheta2_minus^2)
-    theta2_plus = atan2(sintheta2_plus,costheta2_plus)
-    theta2_minus = atan2(sintheta2_minus,costheta2_minus)
-    theta4_plus = phi-theta2_plus-theta3_plus
-    theta4_minus = phi-theta2_minus-theta3_minus
+    sintheta2_plus = sqrt(1-costheta2_plus^2);
+    sintheta2_minus = sqrt(1-costheta2_minus^2);
+    theta2_plus = atan2(sintheta2_plus,costheta2_plus);
+    theta2_minus = atan2(sintheta2_minus,costheta2_minus);
+    theta4_plus = phi-theta2_plus-theta3_plus;
+    theta4_minus = phi-theta2_minus-theta3_minus;
     IK = [theta1, theta2_plus+theta_o, theta3_plus-theta_o, theta4_plus; theta1, theta2_minus+theta_o, theta3_minus-theta_o, theta4_minus];
 end
 
