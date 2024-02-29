9dfff26aa7c4744762a0c69571e9feca7472b88b
james taylor <jameset701@gmail.com>
2024-02-29 14:58:49 +0000

    fixed ik issue

--- Task1.m
+++ Task1.m
@@ -11,7 +11,7 @@ theta4 = [zeros(1,441),0:0.01:1.8837,1.8837:-0.01:-2.15 , -2.15:0.01:0];
 plot_OpenManipX(theta1, theta2, theta3, theta4,0);
 %}
 %%% Backwards kinematic simuation 
-
+%{
 %%% xy-plane
 angles_xy=[choose_kinematic(inverse_kinematic(0.1,-0.1,0,-pi/2)); 
         choose_kinematic(inverse_kinematic(0.2,-0.1,0,-pi/2));
@@ -48,6 +48,12 @@ plot_OpenManipX(angles_yz(:,1), angles_yz(:,2), angles_yz(:,3), angles_yz(:,4),1
 % j4=rad_to_j4(x2(4))
 % plot_OpenManipX(x2(1),x2(2),x2(3),x2(4),0)
 %plot_OpenManipX(x2(2,1),x2(2,2),x2(2,3),x2(2,4),0)
+xyz = coords_to_meters(3,-8,0.08);
+position = inverse_kinematic(xyz(1),xyz(2),xyz(3),-pi/2);
+position_kinematic = choose_kinematic(position);
+plot_OpenManipX(position(1,1),position(1,2),position(1,3),position(1,4),0);
+plot_OpenManipX(position(2,1),position(2,2),position(2,3),position(2,4),0);
+plot_OpenManipX(position_kinematic(1),position_kinematic(2),position_kinematic(3),position_kinematic(4),0);
 function plot_OpenManipX(theta1, theta2, theta3, theta4, draw_lines)
     % DH parameters for OpenManipulator-X
     % alpha a d theta (degrees)
@@ -156,23 +162,23 @@ function DH = DH_table(theta1, theta2, theta3, theta4)
 end
 
 function IK_final = choose_kinematic(joint_angles)
-    j1=rad_to_j1(joint_angles(1,1));
-    j2=rad_to_j2(joint_angles(1,2)); 
-    j3=rad_to_j3(joint_angles(1,3));
-    j4=rad_to_j4(joint_angles(1,4));
-    if (j2 < 760) || (j2 > 3290) || (j3 < 695) || (j3 > 3060) || (j4 < 820) || (j4 > 3450) || any(isnan(joint_angles(1,:)))
-        %warning('IK using positive cosine component out of range')
-        j1=rad_to_j1(joint_angles(2,1));
-        j2=rad_to_j2(joint_angles(2,2)); 
-        j3=rad_to_j3(joint_angles(2,3));
-        j4=rad_to_j4(joint_angles(2,4));
-        if (j2 < 760) || (j2 > 3290) || (j3 < 695) || (j3 > 3060) || (j4 < 820) || (j4 > 3450) || any(isnan(joint_angles(2,:)))
+    j1=rad_to_j1(joint_angles(2,1));
+    j2=rad_to_j2(joint_angles(2,2)); 
+    j3=rad_to_j3(joint_angles(2,3));
+    j4=rad_to_j4(joint_angles(2,4));
+    if (j2 < 760) || (j2 > 3290) || (j3 < 695) || (j3 > 3060) || (j4 < 820) || (j4 > 3450)
+        warning('IK using elbow up out of range')
+        j1=rad_to_j1(joint_angles(1,1));
+        j2=rad_to_j2(joint_angles(1,2)); 
+        j3=rad_to_j3(joint_angles(1,3));
+        j4=rad_to_j4(joint_angles(1,4));
+        if (j2 < 760) || (j2 > 3290) || (j3 < 695) || (j3 > 3060) || (j4 < 820) || (j4 > 3450)
             error('Joint angles are out of range for both IK solvers');
         else
-            IK_final = joint_angles(2,:);
+            IK_final = joint_angles(1,:);
         end
     else
-        IK_final = joint_angles(1,:);
+        IK_final = joint_angles(2,:);
     end
 end
 
@@ -183,16 +189,16 @@ function IK = inverse_kinematic(x_ef,y_ef,z_ef,phi)
     r_ef = sqrt(x_ef^2+y_ef^2);
     r_2 = r_ef-0.126*cos(phi);
     z_2 = z_ef-0.126*sin(phi);
-    theta3_plus = acos((r_2^2+z_2^2-0.13^2-0.124^2)/(2*0.13*0.124))
-    theta3_minus = -acos((r_2^2+z_2^2-0.13^2-0.124^2)/(2*0.13*0.124))
+    theta3_plus = acos((r_2^2+z_2^2-0.13^2-0.124^2)/(2*0.13*0.124));
+    theta3_minus = -acos((r_2^2+z_2^2-0.13^2-0.124^2)/(2*0.13*0.124));
     costheta2_plus = (r_2*(0.13+0.124*cos(theta3_plus))+z_2*0.124*sin(theta3_plus))/(r_2^2+z_2^2);
     costheta2_minus = (r_2*(0.13+0.124*cos(theta3_minus))+z_2*0.124*sin(theta3_minus))/(r_2^2+z_2^2);
-    sintheta2_plus = -sqrt(1-costheta2_plus^2)
-    sintheta2_minus = sqrt(1-costheta2_minus^2)
-    theta2_plus = atan2(sintheta2_plus,costheta2_plus)
-    theta2_minus = atan2(sintheta2_minus,costheta2_minus)
-    theta4_plus = phi-theta2_plus-theta3_plus
-    theta4_minus = phi-theta2_minus-theta3_minus
+    sintheta2_plus = sqrt(1-costheta2_plus^2);
+    sintheta2_minus = sqrt(1-costheta2_minus^2);
+    theta2_plus = atan2(sintheta2_plus,costheta2_plus);
+    theta2_minus = atan2(sintheta2_minus,costheta2_minus);
+    theta4_plus = phi-theta2_plus-theta3_plus;
+    theta4_minus = phi-theta2_minus-theta3_minus;
     IK = [theta1, theta2_plus+theta_o, theta3_plus-theta_o, theta4_plus; theta1, theta2_minus+theta_o, theta3_minus-theta_o, theta4_minus];
 end
 
